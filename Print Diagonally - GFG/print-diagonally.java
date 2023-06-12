ad9703747ed91511b50e63f57c9ff7a079a9fcf4
Mohit singh <88044075+mohitsingh785@users.noreply.github.com>
2023-06-12 15:46:08 +0530

    Added solution - LeetHub

---
+++ Print Diagonally - GFG/print-diagonally.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            
+            int matrix[][] = new int[n][n];
+            
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                 matrix[i][j] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.downwardDiagonal(n, matrix);
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static ArrayList<Integer> downwardDiagonal(int N, int A[][])
+    {
+        // code here 
+        
+        ArrayList<Integer> list=new ArrayList<>();
+        
+      
+      
+        
+        
+        int l=1;
+        int k=0;
+        while(k<A.length){
+            
+            
+            int j=k;
+            int i=0;
+            while(j>=0){
+                
+                
+                
+                list.add(A[i][j]);
+                i++;
+                
+                j--;
+                
+                
+            }
+            
+            
+            k++;
+           
+            
+            
+            
+        }
+              while(l<A.length){
+            
+             
+            int j=k-1;
+            int i=l;
+            while(j>=0 && i<A.length){
+                
+                
+                
+                list.add(A[i][j]);
+                i++;
+                
+                j--;
+                
+                
+            }
+            
+            
+           l++;
+            
+        }
+      
+        
+      
+        return list;
+    }
+}

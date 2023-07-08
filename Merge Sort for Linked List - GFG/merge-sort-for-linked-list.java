b408acbd3f3a31ea2499ee118a5fd88c69a08f43
Mohit singh <88044075+mohitsingh785@users.noreply.github.com>
2023-07-08 17:25:04 +0530

    Added solution - LeetHub

---
+++ Merge Sort for Linked List - GFG/merge-sort-for-linked-list.java
@@ -0,0 +1,139 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().mergeSort(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node sortedMerge(Node a,Node b)
+    {
+    Node result = null;
+        /* Base cases */
+        if (a == null)
+            return b;
+        if (b == null)
+            return a;
+ 
+        /* Pick either a or b, and recur */
+        if (a.data <= b.data) {
+            result = a;
+            result.next = sortedMerge(a.next, b);
+        }
+        else {
+            result = b;
+            result.next = sortedMerge(a, b.next);
+        }
+        return result;
+    }
+ 
+   static Node mergeSort(Node h)
+    {
+        // Base case : if head is null
+        if (h == null || h.next == null) {
+            return h;
+        }
+ 
+        // get the middle of the list
+        Node middle = getMiddle(h);
+        Node nextofmiddle = middle.next;
+ 
+        // set the next of middle node to null
+        middle.next = null;
+ 
+        // Apply mergeSort on left list
+        Node left = mergeSort(h);
+ 
+        // Apply mergeSort on right list
+        Node right = mergeSort(nextofmiddle);
+ 
+        // Merge the left and right lists
+        Node sortedlist = sortedMerge(left, right);
+        return sortedlist;
+    }
+ 
+    // Utility function to get the middle of the linked list
+    public static Node getMiddle(Node head)
+    {
+        if (head == null)
+            return head;
+ 
+        Node slow = head, fast = head;
+ 
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+
+
